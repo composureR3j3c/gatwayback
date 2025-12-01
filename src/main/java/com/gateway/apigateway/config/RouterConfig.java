6abcb363a2e5db909e33bc7c20c3d93b0fd6fbd1
bereket <gezahegnebereket@gmail.com>
2025-12-01 19:13:10 +0300

    routes fixed

--- src/main/java/com/gateway/apigateway/config/RouterConfig.java
+++ src/main/java/com/gateway/apigateway/config/RouterConfig.java
@@ -18,8 +18,8 @@ public class RouterConfig {
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return RouterFunctions
-                .route(RequestPredicates.path("/admin/**"), request -> ServerResponse.notFound().build())
-                .andRoute(RequestPredicates.path("/**").and(req -> !req.path().startsWith("/admin") &&
+                // .route(RequestPredicates.path("/admin/**"), request -> ServerResponse.notFound().build())
+                .route(RequestPredicates.path("/**").and(req -> !req.path().startsWith("/admin") &&
                 !req.path().startsWith("/actuator")),
                         req -> handler.handle(req.exchange()).then(ServerResponse.ok().build()));
     }
